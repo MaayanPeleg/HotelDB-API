2f73be402c2c3ba36bbdef875effbd2466e0448a
Maayan Peleg <maayan.peleg87@gmail.com>
2023-03-16 15:00:35 +0000

    build: seperated jobs into 2 files to only run if change in dir

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 # run with "flask --app server run --debug" or "python server.py"
 #In deployement use "gunicorn server:app -b 0.0.0.0:8000" 
-import mysql.connector as mysql #pip install mysql-connector-python
-from flask import Flask #pip install flask
+import mysql.connector as mysql 
+from flask import Flask 
 import os
 
 app = Flask(__name__)
